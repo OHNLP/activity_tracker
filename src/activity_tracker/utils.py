5fe1832252d32d4f74d28b35f76c77d3d79df5a1
Jaerong Ahn <jaerongahn@gmail.com>
2025-06-10 19:04:57 -0500

    chore: comment out unused functions in utils.py

--- src/activity_tracker/utils.py
+++ src/activity_tracker/utils.py
@@ -4,10 +4,12 @@ import re
 from collections import defaultdict
 
 import pandas as pd
-import pyodbc
+
+# import pyodbc
 import yaml
 from dotenv import load_dotenv
-from pyspark.sql import DataFrame, SparkSession
+
+# from pyspark.sql import DataFrame, SparkSession
 
 # Load environment variables once
 load_dotenv()
@@ -20,56 +22,56 @@ JDBC_DRIVER = os.getenv("JDBC_DRIVER")
 JDBC_URL = f"jdbc:jtds:sqlserver://{SERVER}:1433/{DATABASE};domain={DOMAIN}"
 
 
-def get_spark_session(app_name: str = "SQLServer-JTDS-Query") -> SparkSession:
-    """Initialize and return a Spark session configured for JDBC with JTDS."""
-    return (
-        SparkSession.builder.appName(app_name)
-        .config("spark.driver.extraClassPath", JDBC_DRIVER)
-        .getOrCreate()
-    )
+# def get_spark_session(app_name: str = "SQLServer-JTDS-Query") -> SparkSession:
+#     """Initialize and return a Spark session configured for JDBC with JTDS."""
+#     return (
+#         SparkSession.builder.appName(app_name)
+#         .config("spark.driver.extraClassPath", JDBC_DRIVER)
+#         .getOrCreate()
+#     )
 
 
-def query_with_spark(query: str, spark: SparkSession = None) -> DataFrame:
-    """Run a SQL query against SQL Server using PySpark via JTDS JDBC driver."""
-    if not spark:
-        spark = get_spark_session()
-    query = f"({query}) AS sub"
+# def query_with_spark(query: str, spark: SparkSession = None) -> DataFrame:
+#     """Run a SQL query against SQL Server using PySpark via JTDS JDBC driver."""
+#     if not spark:
+#         spark = get_spark_session()
+#     query = f"({query}) AS sub"
 
-    return (
-        spark.read.format("jdbc")
-        .option("url", JDBC_URL)
-        .option("dbtable", query)
-        .option("user", USERNAME)
-        .option("password", PASSWORD)
-        .option("driver", "net.sourceforge.jtds.jdbc.Driver")
-        .load()
-    )
-
-
-def read_csv_with_spark(
-    file_path: str | pathlib.Path, spark: SparkSession = None
-) -> DataFrame:
-    """Read a CSV file using Spark with header and inferred schema."""
-    if not spark:
-        spark = get_spark_session()
-    return (
-        spark.read.option("header", "true")
-        .option("inferSchema", "true")
-        .csv(str(file_path))
-    )
+#     return (
+#         spark.read.format("jdbc")
+#         .option("url", JDBC_URL)
+#         .option("dbtable", query)
+#         .option("user", USERNAME)
+#         .option("password", PASSWORD)
+#         .option("driver", "net.sourceforge.jtds.jdbc.Driver")
+#         .load()
+#     )
 
 
-def create_connection() -> pyodbc.Connection:
-    """Create a domain-authenticated connection to SQL Server using FreeTDS."""
-    return pyodbc.connect(
-        f"DRIVER=FreeTDS;"
-        f"SERVER={SERVER};"
-        f"DATABASE={DATABASE};"
-        f"UID={DOMAIN}\\{USERNAME};"
-        f"PWD={PASSWORD};"
-        f"PORT=1433;"
-        f"ntlmv2*=yes;"
-    )
+# def read_csv_with_spark(
+#     file_path: str | pathlib.Path, spark: SparkSession = None
+# ) -> DataFrame:
+#     """Read a CSV file using Spark with header and inferred schema."""
+#     if not spark:
+#         spark = get_spark_session()
+#     return (
+#         spark.read.option("header", "true")
+#         .option("inferSchema", "true")
+#         .csv(str(file_path))
+#     )
+
+
+# def create_connection() -> pyodbc.Connection:
+#     """Create a domain-authenticated connection to SQL Server using FreeTDS."""
+#     return pyodbc.connect(
+#         f"DRIVER=FreeTDS;"
+#         f"SERVER={SERVER};"
+#         f"DATABASE={DATABASE};"
+#         f"UID={DOMAIN}\\{USERNAME};"
+#         f"PWD={PASSWORD};"
+#         f"PORT=1433;"
+#         f"ntlmv2*=yes;"
+#     )
 
 
 def load_data_dictionary(excel_path: str | pathlib.Path) -> dict:
