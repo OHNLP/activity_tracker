4d324772944d58c4202ee102b011552190602b41
Jaerong Ahn <jaerongahn@gmail.com>
2025-07-20 01:58:25 -0500

    feat: :sparkles: add DailyMeasurement table

--- src/activity_tracker/pipeline/measurement.py
+++ src/activity_tracker/pipeline/measurement.py
@@ -1,9 +1,5 @@
-import pathlib
-
 import datajoint as dj
-import pandas as pd
 
-from .. import utils
 from . import get_schema_name
 from .subject import Subject
 from .visit import Visit
@@ -32,3 +28,27 @@ class Frailty(dj.Imported):
     ffp_status=null: varchar(32) # "frail", "prefrail", "robust"
     ffp_status_binary=null: varchar(32) # Combine 'prefrail' and 'robust' into 'no_frail'
     """
+
+    def make(self, key):
+        pass
+
+
+@schema
+class DailyMeasurement(dj.Imported):
+    """
+    Daily measurement from the activity tracker.
+    """
+
+    definition = """
+    # Daily measurement from the activity tracker.
+    -> Subject
+    date: date
+    ---
+    total_steps=null: int
+    total_distance=null: float
+    sedentary_minutes=null: int
+    calories_bmr=null: int
+    """
+
+    def make(self, key):
+        pass
